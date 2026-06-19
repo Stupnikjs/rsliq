7be38c791add77e8149d28c2ad3273350105807b
Stupnikjs <n.boudier.ph@gmail.com>
2026-06-19 21:42:26 +0200

    ok

--- src/onchain/encode.rs
+++ src/onchain/encode.rs
@@ -1,9 +1,7 @@
+use alloy::primitives::{keccak256, Address, Bytes, U256};
+use alloy::sol_types::{SolType, SolValue};
+use alloy::sol_types::sol_data;
 
-
-use alloy::primitives::{keccak256, Address, Bytes, B256, U256};
-use alloy::sol_types::{SolValue, Token};
-
-/// Compute 4-byte function selector
 pub fn selector(signature: &str) -> [u8; 4] {
     let hash = keccak256(signature.as_bytes());
     let mut sel = [0u8; 4];
@@ -11,38 +9,21 @@ pub fn selector(signature: &str) -> [u8; 4] {
     sel
 }
 
-/// Encode calldata: selector + abi-encoded args
-pub fn encode_calldata(selector: [u8; 4], args: &[Token]) -> Bytes {
-    let mut data = Vec::with_capacity(4 + args.abi_encode().len());
-    data.extend_from_slice(&selector);
+pub fn encode_calldata(sel: [u8; 4], args: &impl SolValue) -> Bytes {
+    let mut data = Vec::with_capacity(4 + args.abi_encoded_size());
+    data.extend_from_slice(&sel);
     data.extend(args.abi_encode());
     data.into()
 }
 
-/// Decode ABI-encoded return value
-pub fn decode_single_uint256(data: &Bytes) -> Result<U256, Box<dyn std::error::Error>> {
-    let tokens = Token::decode_sequence(data, false)?;
-    if let Token::Uint(val) = &tokens[0] {
-        Ok(*val)
-    } else {
-        Err("expected uint256".into())
-    }
+pub fn decode_uint(data: &[u8]) -> Result<U256, Box<dyn std::error::Error>> {
+    Ok(sol_data::Uint::<256>::abi_decode(data)?)
 }
 
-pub fn decode_single_string(data: &Bytes) -> Result<String, Box<dyn std::error::Error>> {
-    let tokens = Token::decode_sequence(data, false)?;
-    if let Token::String(s) = &tokens[0] {
-        Ok(s.clone())
-    } else {
-        Err("expected string".into())
-    }
+pub fn decode_address(data: &[u8]) -> Result<Address, Box<dyn std::error::Error>> {
+    Ok(sol_data::Address::abi_decode(data)?)
 }
 
-pub fn decode_single_address(data: &Bytes) -> Result<Address, Box<dyn std::error::Error>> {
-    let tokens = Token::decode_sequence(data, false)?;
-    if let Token::Address(addr) = &tokens[0] {
-        Ok(*addr)
-    } else {
-        Err("expected address".into())
-    }
+pub fn decode_string(data: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(sol_data::String::abi_decode(data)?)
 }
